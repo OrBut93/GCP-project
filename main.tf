3bd417e229b32185f375631e6564646950ace7d1
or <orow1993@gmail.com>
2024-09-10 15:08:09 +0300

    create course folder

--- main.tf
+++ main.tf
@@ -2,5 +2,23 @@ provider "google" {
     credentials = file(var.folder_service_account_key_path)
     project = "management-project" // the management project! 
     region = var.region
-  
+}
+
+data "external" "students"{
+   program = ["python3", "${path.module}/read_csv.py", "students.csv"] 
+}
+
+#Iterate over each student and create resources
+resource "google_project" "student_project" {
+  for_each = { for student in data.data.external.students.result.students :  "${student["first_name"]}-${student["last_name"]}" => student }
+
+  name       = "${each.value.first_name}-${each.value.last_name}"
+  project_id = lower("${each.value.first_name}-${each.value.last_name}-project")
+  org_id     = "<your_org_id>"  # Replace with your organization ID
+  billing_account = "<your_billing_account>"  # Replace with your billing account ID
+}
+
+resource "google_folder" "course_folder" {
+    display_name = var.course_folder_name
+    parent = "organization/	912009230618" // Bynet organization id
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -4,3 +4,5 @@
 
 # backup project - service account key path
 folder_service_account_key_path= "/path/to/your/service-account-key.json"  # Set default or provide value via CLI
+
+course_folder_name = "course-folder"
